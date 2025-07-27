f034d5661c187f3b7d7360805515c4d3f2e3bdbc
Md Arman Ali <mdarmanali070@gmail.com>
2025-07-27 15:26:14 +0600

    hh

--- resources/views/frontend/v-2/master.blade.php
+++ resources/views/frontend/v-2/master.blade.php
@@ -15,6 +15,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','{{$code->gtm_id ?? 'GTM-XXXXXXX'}}');</script>
 <!-- End Google Tag Manager -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
 </head>
 
 <body>
